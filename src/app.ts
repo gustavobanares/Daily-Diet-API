eecfb66d144f56a7d0db7c490155e7ed6878d2fd
*Gustavo <gustavobbacco@gmail.com>
2025-02-04 23:48:38 -0300

    feat: setup to executing project

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+
+import { mealRoutes } from "./routes/meal";
+
+export const app = fastify();
+
+app.register(cookie);
+
+app.register(mealRoutes, {
+  prefix: "meals",
+});
