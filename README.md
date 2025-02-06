f56bb88f9d843780914d1414234eceed43600a82
*Gustavo <gustavobbacco@gmail.com>
2025-02-05 23:38:39 -0300

    feat: create meal with your unique id

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Requisitos Funcionais (RF)
 
-[] Deve ser possível criar um usuário;
-[] Deve ser possível identificar o usuário entre as requisições;
-[] Deve ser possível registrar uma refeição feita, com as seguintes informações:
-[] Nome;
-[] Descrição;
-[] Data e Hora;
-[] Está dentro ou não da dieta;
+[x] Deve ser possível criar um usuário;
+[x] Deve ser possível identificar o usuário entre as requisições;
+[x] Deve ser possível registrar uma refeição feita, com as seguintes informações:
+[x] Nome;
+[x] Descrição;
+[x] Data e Hora;
+[x] Está dentro ou não da dieta;
+
 [] Deve ser possível editar uma refeição, podendo alterar todos os dados acima;
 [] Deve ser possível apagar uma refeição;
-[] Deve ser possível listar todas as refeições de um usuário;
+[x] Deve ser possível listar todas as refeições de um usuário;
 [] Deve ser possível visualizar uma única refeição;
 [] Deve ser possível recuperar as métricas de um usuário:
 [] Quantidade total de refeições registradas;
@@ -21,7 +22,7 @@
 # Requisitos Não Funcionais (RNF)
 [] O sistema deve utilizar autenticação para identificar o usuário entre as requisições;
 [] O tempo de resposta da API deve ser inferior a 1 segundo para as principais operações;
-[] O sistema deve ser desenvolvido utilizando Node.js e TypeScript;
+[x] O sistema deve ser desenvolvido utilizando Node.js e TypeScript;
 
 
 # Regras de Negócio (RN)
--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from "fastify";
-import { randomUUID } from "node:crypto";
+import { randomInt, randomUUID } from "node:crypto";
 import { z } from "zod";
 import { knex } from "../database";
 import { checkSessionIdExists } from "../middlewares/check-session-id-exists";
+
 
 export async function mealRoutes(app: FastifyInstance) {
   app.get(
@@ -13,11 +14,53 @@ export async function mealRoutes(app: FastifyInstance) {
     async (request, reply) => {
       const { sessionId } = request.cookies;
 
-      const meals = await knex("meals").where("sessionId", sessionId).select();
+      const meals = await knex("meals").where("session_id", sessionId).select()
+      const mappedMeals = meals.map((meal) =>({
+        ...meal,
+        is_on_diet: meal.is_on_diet ? 'Sim' : "Não",
+      }))
 
       return {
-        meals,
+        meals: mappedMeals,
       }
     }
   );
+
+  app.post("/", async (request, reply) => {
+    const createMealBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      date_time: z.preprocess((arg) => {
+        if (typeof arg === "string" || arg instanceof Date) {
+          return new Date(arg);
+        }
+        return arg;
+      }, z.date()), // Aqui garantimos que será uma data válida
+      is_on_diet: z.boolean(),
+    });
+    const { name, description, date_time, is_on_diet } = createMealBodySchema.parse(request.body);
+
+    let sessionId = request.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.cookie("sessionId", sessionId, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
+    await knex("meals").insert({
+      id: randomUUID(),
+      name,
+      description,
+      date_time: new Date(date_time).toISOString(),
+      is_on_diet,
+      session_id: sessionId,
+      created_at: new Date().toISOString(),
+    });
+
+    return reply.status(201).send();
+  });
 }
